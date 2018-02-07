acec96d72e91895afa7ce0cbe700b3db5125d04a
Tomasz Kielar <tkielar@pgs-soft.com>
2018-02-07 14:36:24 +0100

    Claims view and login/logout actions

--- PGS.Azure.ActiveDirectory/Controllers/HomeController.cs
+++ PGS.Azure.ActiveDirectory/Controllers/HomeController.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace PGS.Azure.ActiveDirectory.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index() => View();
+
+        [Authorize]
+        public IActionResult Profile() => View(User.Claims);
+
+        public IActionResult SignIn() => Challenge();
+
+        public IActionResult SignOut() => SignOut(new AuthenticationProperties());
     }
 }
